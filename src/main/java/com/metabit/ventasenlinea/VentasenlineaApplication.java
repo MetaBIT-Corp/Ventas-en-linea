ccbfc7c1fed1c9c86ac7308a667c05a3bc4820ee
Enrique Menjívar <mt16007@ues.edu.sv>
2019-10-15 15:43:07 -0600

    Commit inicial, Sistema para ventas en linea

---
+++ src/main/java/com/metabit/ventasenlinea/VentasenlineaApplication.java
@@ -0,0 +1,13 @@
+package com.metabit.ventasenlinea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasenlineaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasenlineaApplication.class, args);
+	}
+
+}
