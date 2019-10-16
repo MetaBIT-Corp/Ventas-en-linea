9100b0bd45805fa54e9a59808be0c129a916da42
Enrique Menjívar <mt16007@ues.edu.sv>
2019-10-16 00:04:07 -0600

    H10 crear cuenta de usuario cliente

---
+++ src/main/java/com/metabit/ventasenlinea/service/ClienteService.java
@@ -0,0 +1,7 @@
+package com.metabit.ventasenlinea.service;
+
+import com.metabit.ventasenlinea.entity.Cliente;
+
+public interface ClienteService {
+	public abstract void createCliente(Cliente cliente);
+}
